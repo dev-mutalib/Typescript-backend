dd64d3ca75984390ae80c01f9f749655db353fb8
Abdul Mutalib <mutalib92847@gmail.com>
2026-03-23 23:24:39 +0530

    create MongoDB connection setup

---
+++ src/config/db.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import type { Mongoose } from 'mongoose';
+
+const connectDB = async (): Promise<void> => {
+  try {
+    const mongoURI = process.env.MONGO_URI;
+
+    if (!mongoURI) {
+      throw new Error('MONGO_URI is not defined in the environment variables');
+    }
+    const conn: any = await mongoose.connect(mongoURI);
+
+    console.log(`MongoDB Connected Successfully`)
+    console.log(`MongoDB host : ${conn.connection.host}`)
+  } catch (error) {
+     console.log('MongoDB connection failed', error)
+     process.exit(1)
+  }
+};
